04f3c642b5b6e9d18a5f828a727f60f2ec6ccdf9
Laura Don Diego <lauradondiego@Leonards-Air.home>
2019-07-11 11:10:44 -0400

    finished articles portion of project

--- Article/Article.js
+++ Article/Article.js
@@ -1,23 +1,292 @@
-// Because classes are not hoisted you will need to start your code at the bottom of the page.
-// Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
-  };
+/* This is the data we will be using to create our article components */
+/* Look over this data, then proceed to line 91*/
+const data = [
+  //array of objects
+  {
+    title: 'Lambda School Students: "We\'re the best!"',
+    date: 'Nov 5th, 2018',
+    firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+        moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+        watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+        Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+        jar twi'lek jinn leia jango skywalker mon.`,
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
+        windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
+        mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
+        k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
+        solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
+        owen jinn tatooine sith organa.`,
+
+    thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+        naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+        han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+  },
+  {
+    title: 'Javascript and You, ES6',
+    date: 'May 7th, 2019',
+    firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
+        Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
+        snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
+        yew pumpkin juice phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia Pigwidgeon
+        knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed.
+        Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks`,
+
+    secondParagraph: `Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder
+        wand, spectacles House Cup Bertie Bott’s Every Flavor Beans Impedimenta. Stunning spells tap-dancing spider Slytherin’s Heir
+        mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma
+        and Parvati Sorting Hat Minister of Magic blue turban remember my last.`,
+
+    thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
+        Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
+        roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
+        sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
+  },
+  {
+    title: 'React vs Angular vs Vue',
+    date: 'June 7th, 2019',
+    firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Squirtle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wartortle Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Blastoise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Caterpie Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Metapod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Butterfree
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit.`,
+
+    secondParagraph: `Pidgey Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Fearow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ekans Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Arbok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pikachu Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Raichu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sandshrew Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sandslash Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
+
+    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
+        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
+        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
+  },
+  {
+    title: 'Professional Software Development in 2019',
+    date: 'Jan 1st, 2019',
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Laura',
+    date: 'Jan 1st, 2019',
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   }
+];
+console.log(data.map(myObject => myObject.date))
+console.log(data.map(myObject => myObject.title))
+console.log(data.map(myObject => myObject.firstParagraph))
+console.log(data.map(myObject => myObject.secondParagraph))
+console.log(data.map(myObject => myObject.thirdParagraph))
+
+
+function createPanel(date, title, firstParagraph, secondParagraph, thirdParagraph) {
+
+  // define new elements
+  // header, p, p, button
+  const article = document.createElement('div')
+  const header = document.createElement('h2');
+  const datePara = document.createElement('p')
+  const button = document.createElement('button')
+  const firstPara = document.createElement('p')
+  const secondPara = document.createElement('p')
+  const thirdPara = document.createElement('p')
+
+console.log(article, articles, header, datePara, firstPara, secondPara, thirdPara, button)
+  article.appendChild(header);
+  article.appendChild(datePara);
+  article.appendChild(firstPara);
+  article.appendChild(secondPara);
+  article.appendChild(thirdPara);
+  article.appendChild(button);
+
+  datePara.textContent = date
+  button.textContent = 'Open'
+  firstPara.textContent = firstParagraph
+  secondPara.textContent = secondParagraph
+  thirdPara.textContent = thirdParagraph
+  header.textContent = title
+
+  //classlist.add next and text content
+
+  datePara.classList.add('date')
+  article.classList.add('article')
+  button.classList.add('expandButton')
+  button.addEventListener('click', (event) => {
+    // article.classList.add('article-open')
+    article.classList.toggle('article-open')
+  })
+
+return article
+
 }
+const articles = document.querySelector('.articles');
+
+const date = data.map(myObject => myObject.date)
+const title = data.map(myObject => myObject.title)
+const firstParagraph = data.map(myObject => myObject.firstParagraph)
+const secondParagraph = data.map(myObject => myObject.secondParagraph)
+const thirdParagraph = data.map(myObject => myObject.thirdParagraph)
+
+articles.appendChild(createPanel(date[0], title[0], firstParagraph[0], secondParagraph[0], thirdParagraph[0]));
+articles.appendChild(createPanel(date[1], title[1], firstParagraph[1], secondParagraph[1], thirdParagraph[1]));
+articles.appendChild(createPanel(date[2], title[2], firstParagraph[2], secondParagraph[2], thirdParagraph[2]));
+articles.appendChild(createPanel(date[3], title[3], firstParagraph[3], secondParagraph[3], thirdParagraph[3]));
+articles.appendChild(createPanel(date[4], title[4], firstParagraph[4], secondParagraph[4], thirdParagraph[4]));
+
+
+
+  // const article = document.createElement('div');
+  // const articleHeader = document.createElement('h2');
+  // const articleDate = document.createElement('p');
+  // const articleP1 = document.createElement('p');
+  // const articleP2 = document.createElement('p');
+  // const articleP3 = document.createElement('p');
+  // const articleButton = document.createElement('button');
+ 
+  // create structure of elements
+  // article.appendChild(articleHeader);
+  // article.appendChild(articleDate);
+  // article.appendChild(articleP1);
+  // article.appendChild(articleP2);
+  // article.appendChild(articleP3);
+  // article.appendChild(articleButton);
+ 
+  // assign class names
+
+
+//   article.classList.add('article');
+//   articleDate.classList.add('date');
+//   articleButton.classList.add('expandButton');
+ 
+//   // set text content
+//   articleHeader.textContent = title;
+//   articleDate.textContent = date;
+//   articleP1.textContent = firstParagraph;
+//   articleP2.textContent = secondParagraph;
+//   articleP3.textContent = thirdParagraph;
+//   articleButton.textContent = "Expand";
+ 
+//   // button event
+//   articleButton.addEventListener('click', event => {
+//     // check if button has been clicked
+//     console.log("button clicked", event.target);
+ 
+//     // toggle article-open class on/off
+//     article.classList.toggle('article-open');
+ 
+//     // check if article-open class is on/off
+//     console.log("article with class", article);
+//   })
+ 
+//   return article;
+//  }
+ 
+//  const articles = document.querySelector('.articles');
+ 
+//  // map over the data, creating a component for each oject
+//  data.forEach(element => {
+//   // check what the element is
+//   console.log(“current element:“, element);
+ 
+//   // append the element items to the articles parent div
+//   articles.appendChild(createPanel(element.date, element.title, element.firstParagraph, element.secondParagraph, element.thirdParagraph));
+//  });
+
+
+
+
+
+
+
+
+
+
+
+
+/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+  
+  <div class="article">
+    <h2>{title of the article}</h2>
+    <p class="date">{date of the article}</p>
+    {three separate paragraph elements}
+    <span class='expandButton'></span>
+  </div>
+  Hint: You will need to use createElement more than once here!
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each peice of the data object above.
+  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+  Step 3: return the entire component.
+  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
+*/
+// const accordion = document.querySelector('.accordion')
+
+// function newsPanel(title) {
+//   //Create the Elements
+//   const articles = document.createElement('div');
+//   const article = document.createElement('div');
+//   const articleTitle = document.createElement('h2');
+//   const articleDate = document.createElement('p');
+//   const buttons = document.createElement('.expandButton')
+
+//   articles.appendChild(article);
+//   article.appendChild(articleTitle);
+//   return article;
+// }
+
+
+
+
+
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement = domElement;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton = domElement.querySelector('.expandButton');
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     this.expandButton.textContent = "expand";
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.expandButton.addEventListener('click', () => this.expandArticle());
+//   };
+
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     this.domElement.classList.toggle('article-open');
+//   }
+// }
 
 /* START HERE: 
 
@@ -25,9 +294,45 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate
 over the articles NodeList and create a new instance of Article by passing in each article
 as a parameter to the Article class.
-*/
+// */
 
-let articles = document.querySelectorAll('.article');
-articles.forEach((myArticle) => {
-  return new Article(myArticle);
-});
+// let articles = document.querySelectorAll('.article');
+// articles.forEach((myArticle) => {
+//   return new Article(myArticle);
+// });
+
+
+
+// // Because classes are not hoisted you will need to start your code at the bottom of the page.
+// // Look for the comment "START HERE"
+
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement = domElement;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton = domElement.querySelector('.expandButton');
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     this.expandButton.textContent = "expand";
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.expandButton.addEventListener('click', () => this.expandArticle());
+//   };
+
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     this.domElement.classList.toggle('article-open');
+//   }
+// }
+
+// /* START HERE: 
+
+// - Select all classes named ".article" and assign that value to the articles variable.  
+// - With your selection in place, now chain .forEach() on to the articles variable to iterate
+// over the articles NodeList and create a new instance of Article by passing in each article
+// as a parameter to the Article class.
+// */
+
+// let articles = document.querySelectorAll('.article');
+// articles.forEach((myArticle) => {
+//   return new Article(myArticle);
+// });
